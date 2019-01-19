0778cc612ed6200ca8979cdd564481b4860bb45e
Charlie <cbenz88@gmail.com>
2019-01-19 08:40:15 -0500

    changed port number

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var app = require('./server.js');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3014;
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
