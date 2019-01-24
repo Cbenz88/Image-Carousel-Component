e3944f99edd691d0f4fafe940cbf9d8731d2d5f6
Charlie <cbenz88@gmail.com>
2019-01-24 16:05:01 -0500

    moved connection to controller

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
 const app = require('./server.js');
 const port = process.env.PORT || 3014;
-const redis = require('redis');
-const REDIS_PORT = process.env.REDIS_PORT;
-const REDIS_HOST = process.env.REDIS_HOST;
-const client = redis.createClient(REDIS_PORT, REDIS_HOST);
+// const redis = require('redis');
+// const REDIS_PORT = process.env.REDIS_PORT;
+// const REDIS_HOST = process.env.REDIS_HOST;
+// const client = redis.createClient(REDIS_PORT, REDIS_HOST);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
 
-client.on('connect', function() {
-    console.log('Redis client connected')
-    console.log(client);
-});
+// client.on('connect', function() {
+//     console.log('Redis client connected')
+//     console.log(client);
+// });
 
-client.on('error', function (err) {
-    console.log('Something went wrong ' + err);
-});
+// client.on('error', function (err) {
+//     console.log('Something went wrong ' + err);
+// });
 
-module.exports = client;
--- server/resources/Controllers.js
+++ server/resources/Controllers.js
@@ -1,10 +1,20 @@
 const mysql = require('../db/index.js');
 const Listing = require('./Models.js');
 const request = require('superagent');
-const client = require('../index.js');
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_HOST = process.env.REDIS_HOST;
+const client = redis.createClient(REDIS_PORT, REDIS_HOST);
 
 module.exports.cache = function(req, res, next) {
     var listingNumber = req.params.number;
+    client.on('connect', function() {
+        console.log('Redis client connected')
+        console.log(client);
+    });
+    client.on('error', function (err) {
+        console.log('Something went wrong ' + err);
+    });
     console.log(client);
     client.get(listingNumber, function (err, data) {
         if (data != null) {
