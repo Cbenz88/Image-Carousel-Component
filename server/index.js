cf839bbf8d770858218242e70e8ee82a68d780b0
Charlie <cbenz88@gmail.com>
2019-01-24 11:42:14 -0500

    testing redis

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const app = require('./server.js').app;
-const client = require('./server.js').client;
+const app = require('./server.js');
+const client = require('./server.js');
 const port = process.env.PORT || 3014;
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-});
 client.on('connect', function() {
     console.log('Redis client connected');
 });
 
 client.on('error', function (err) {
     console.log('Issue connecting to Redis' + err);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
 });
